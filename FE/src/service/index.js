b4de18ac3307b98b1d348328e8bb1947a50bcec8
ntoneko <nicolastonelli1406@gmail.com>
2019-07-27 15:22:30 -0300

    Big refactor for deployment to Now

--- FE/src/service/index.js
+++ FE/src/service/index.js
@@ -1,8 +1,5 @@
 export const fetchProductsByPage = async page => {
-  // const resp = await fetch(`http://localhost:3001/products?page=${page}`);
-  const resp = await fetch(
-    `https://challenge-api.aerolab.co/products?page=${page}`
-  );
+  const resp = await fetch(`/api/products?page=${page}`);
   const products = await resp.json();
 
   return products;
